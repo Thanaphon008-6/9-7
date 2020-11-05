5455df37c0d28b579ee5457bb31d79a29de07b2f
Thanaphon0086 <geezxcvbnmgee@gmail.com>
2020-11-05 13:41:41 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+float A,B,ans=1;
+float power(float a,int b){
+	int i;
+	for(i=1;i<=b;i++){
+		ans *= a;
+	}
+	return ans;
+}
+int main(void){
+	do{
+		printf("Enter number : ");
+ 		scanf("%f", &A);
+ 		printf("Enter power of number : ");
+ 		scanf("%f", &B);
+ 		int x = B;
+ 		if(B != x || B < 0){
+ 			B = -1;
+ 			printf("Can't do this !!!\n");
+		 }
+	}while(B < 0);
+	power(A ,B);
+	printf("\n%f\n",ans);
+  return 0;
+}
